ca1af906bd40e260d84c2ac5a0ea888e830217ad
Lucas Ribeiro Magalhaes <lucas.magalhaes@murabei.com>
2026-08-04 14:47:46 -0300

    Refactor build process to use Poetry and update versioning scheme. Added pyproject_template.toml for package configuration and removed setup.py and setup_template.py. Introduced CI workflow for automated builds and publishing.

--- build.sh
+++ build.sh
@@ -7,14 +7,17 @@ new_version="$major.$minor.$patch"
 sed -i "s/^VERSION=.*/VERSION=$new_version/" "$VERSION_FILE"
 echo "Updated version to $new_version"
 
-# Set package version
 source VERSION
-sed -e 's#{VERSION}#'"${VERSION}"'#g' setup_template.py > setup.py
+# Detecta branch atual e adiciona -beta se não for main/master
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$CURRENT_BRANCH" != "main" && "$CURRENT_BRANCH" != "master" ]]; then
+  VERSION="${VERSION}-b.0"
+fi
 
-python3 setup.py build sdist bdist_wheel
+sed -e 's#{VERSION}#'"${VERSION}"'#g' pyproject_template.toml > pyproject.toml
 
-cd django_dev
-python mk_docs.py
+rm -R build/
+poetry build
 
 git add --all
 git commit -m "Building a new version ${VERSION}"
